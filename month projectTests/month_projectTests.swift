88c25a6d148de88510bf55c4da8fbd2e22a3aef0
Toby BUi <90307984@ep-student.org>
2020-12-22 10:11:28 -0600

    Initial Commit

---
+++ month projectTests/month_projectTests.swift
@@ -0,0 +1,33 @@
+//
+//  month_projectTests.swift
+//  month projectTests
+//
+//  Created by 90307984 on 12/22/20.
+//
+
+import XCTest
+@testable import month_project
+
+class month_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
